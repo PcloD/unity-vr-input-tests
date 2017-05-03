290fe0a2a2b2d0f7bd9ff638a52f5113feb34371
Duane Bradbury <dibely@hotmail.com>
2017-05-03 16:19:41 +0100

    Added a vr-input-native-test scene that uses Unity's built in VR support for tracking the controllers.

---
+++ Assets/Scripts/VRControllerTrackerNative.cs
@@ -0,0 +1,61 @@
+﻿using UnityEngine;
+using UnityEngine.VR;
+
+public class VRControllerTrackerNative : MonoBehaviour {
+    private static string OPENVR_CONTROLLER_LEFT = "OpenVR Controller - Left";
+    private static string OPENVR_CONTROLLER_RIGHT = "OpenVR Controller - Right";
+    private int controllerIndex = -1;
+    public VRNode vrNode;
+
+	// Use this for initialization
+	void Start () {
+        string[] inputDevices = Input.GetJoystickNames();
+
+        for (int deviceIndex = 0; deviceIndex < inputDevices.Length; deviceIndex++) {
+            string deviceName = inputDevices[deviceIndex];
+            Debug.Log(deviceName);
+
+            string controllerToTestFor = null;
+            if (vrNode == VRNode.LeftHand) {
+                controllerToTestFor = OPENVR_CONTROLLER_LEFT;
+            }
+            else if(vrNode == VRNode.RightHand) {
+                controllerToTestFor = OPENVR_CONTROLLER_RIGHT;
+            }
+
+            if (deviceName.Equals(controllerToTestFor)) {
+                controllerIndex = deviceIndex;
+                break;
+            }
+
+        }
+    }
+	
+	// Update is called once per frame
+	void Update () {
+        if(controllerIndex == -1) {
+            Debug.LogWarning("Controller not found");
+            return;
+        }
+
+        if (!IsControllerStillConnected()) {
+            Debug.LogWarning("Device disconnected");
+            return;
+        }
+
+        // Update game object with the position and rotation of the VR controller
+        Vector3 handPosition = InputTracking.GetLocalPosition(vrNode);
+        //Debug.Log("Hand Position = " + handPosition);
+        gameObject.transform.localPosition = handPosition;
+
+        Quaternion handRotation = InputTracking.GetLocalRotation(vrNode);
+        //Debug.Log("Hand Rotation = " + handRotation);
+        gameObject.transform.localRotation = handRotation;
+    }
+
+    private bool IsControllerStillConnected() {
+        string[] inputDevices = Input.GetJoystickNames();
+        return (inputDevices[controllerIndex] != null);
+    }
+
+}
